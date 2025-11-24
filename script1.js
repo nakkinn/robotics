7b2fb244457b91ee4f4c2d30a513175ad845ee71
nakkinn <81859514+nakkinn@users.noreply.github.com>
2025-11-25 00:05:47 +0900

    Update script1.js

--- script1.js
+++ script1.js
@@ -2,8 +2,14 @@ let x1 = 200; //位置
 let a1 = 0; //加速度
 let v1 = 0; //速度
 let x0 = 0;
+let e0 = 1;
+let sum_e = 0;
 
 const ctx = document.getElementById('myChart').getContext('2d');
+const check0 = document.getElementById('check0');
+const check1 = document.getElementById('check1');
+const check2 = document.getElementById('check2');
+const check3 = document.getElementById('check3');
 
 let labels1 = [];
 for(let i=0; i<100; i++){
@@ -68,48 +74,77 @@ function setup(){
 function draw(){
 
 
-    //描画
+	//描画
 
-    background(240);
-
-
-    translate(width/2, 0);
-
-    noStroke();
-    fill(75, 192, 192);
-    circle(x1, height/2, 20);
-
-    fill(212, 47, 47);
-    circle(x0, height/2, 16);
-
-    if(frameCount % 2 == 0){
-
-        data1[0] = x1;
-        for(let j=data1.length-1; j>0; j--){
-            data1[j] = data1[j-1];
-        }
-
-        data0[0] = x0;
-        for(let j=data0.length-1; j>0; j--){
-            data0[j] = data0[j-1];
-        }
-
-        myChart.data.datasets[0].data = data0;
-        myChart.data.datasets[1].data = data1;
-        myChart.update();
-
-    }
+	background(240);
 
 
+	translate(width/2, 0);
 
-    //処理
+	noStroke();
+	fill(75, 192, 192);
+	circle(x1, height/2, 20);
 
-    a1 = (x0 - x1) * 0.001 - v1 * 0.01;
-    v1 += a1;
-    x1 += v1;
+	fill(212, 47, 47);
+	circle(x0, height/2, 16);
 
-    if(mouseIsPressed){
-        x0 = mouseX - width/2;
-    }
+	if(frameCount % 2 == 0){
+
+		data1[0] = x1;
+		for(let j=data1.length-1; j>0; j--){
+			data1[j] = data1[j-1];
+		}
+
+		data0[0] = x0;
+		for(let j=data0.length-1; j>0; j--){
+			data0[j] = data0[j-1];
+		}
+
+		myChart.data.datasets[0].data = data0;
+		myChart.data.datasets[1].data = data1;
+		myChart.update();
+
+	}
+
+
+
+	//処理
+
+	let p1 = 0;
+	if(check0.checked){
+		p1 = 0.001;
+	}
+
+	//D
+	let d1 = 0;
+	if(check1.checked){
+		d1 = 0.02;
+	}
+
+	//I
+	let i1 = 0;
+	if(check2.checked){
+		i1 = 0.000008;
+		sum_e += (x0 - x1);
+	}
+
+	//外乱
+	let gairan = 0;
+	if(check3.checked){
+		gairan = 0.05;
+	}
+
+
+	a1 = p1 * (x0 - x1) - d1 * (x1-x0-e0) + i1 * sum_e + gairan;
+	e0 = x1 - x0;
+
+	v1 += a1;
+	x1 += v1;
+
+	
+
+	if(mouseIsPressed && mouseX >=0 && mouseX <= width && mouseY >=0 && mouseY <= height){
+		x0 = mouseX - width/2;
+	}
 
 }
